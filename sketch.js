ed267a8d22c426338ae84507686ff7efc5a962e5
spaceresearchmamon <91263325+spaceresearchmamon@users.noreply.github.com>
2021-11-03 14:53:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ var hypnoticBall, database;
 var position;
 
 
-function setup(){
+function setup()
+{
   database = firebase.database();
   console.log(database);
   createCanvas(500,500);
@@ -15,36 +16,44 @@ function setup(){
   hypnoticBallPosition.on("value", readPosition, showError);
 }
 
-function draw(){
+function draw()
+{
   background("white");
   
-    if(keyDown(LEFT_ARROW)){
+    if(keyDown(LEFT_ARROW))
+    {
       writePosition(-1,0);
     }
-    else if(keyDown(RIGHT_ARROW)){
+    else if(keyDown(RIGHT_ARROW))
+    {
       writePosition(1,0);
     }
-    else if(keyDown(UP_ARROW)){
+    else if(keyDown(UP_ARROW))
+    {
       writePosition(0,-1);
     }
-    else if(keyDown(DOWN_ARROW)){
+    else if(keyDown(DOWN_ARROW))
+    {
       writePosition(0,+1);
     }
     drawSprites();
   
 }
 
-function writePosition(x,y){
+function writePosition(x,y)
+{
   
 }
 
-function readPosition(data){
+function readPosition(data)
+{
   position = data.val();
   console.log(position.x);
   hypnoticBall.x = position.x;
   hypnoticBall.y = position.y;
 }
 
-function showError(){
+function showError()
+{
   console.log("Error in writing to the database");
 }
